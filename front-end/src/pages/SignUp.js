d2a317677ba9cde54b1995fc71bf1287e35378fb
daniel <danlikestech@gmail.com>
2022-03-17 08:51:26 -0500

    fixed folder

---
+++ front-end/src/pages/SignUp.js
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+  const [data, setData] = useState({
+    firstName,
+  });
+  return (
+    <div>
+      <h1>SignUp Page</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Sign Up
+        </Button>
+        <Link to="/login">
+          <Button variant="primary" type="submit">
+            Sign In
+          </Button>
+        </Link>
+      </Form>
+    </div>
+  );
+};
+
+export default Signup;
